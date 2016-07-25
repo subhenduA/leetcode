42cb2fa2e0f86d6e96caf73b1f4d78a404dd2a9d
subhenduA <subhendu.aich@gmail.com>
2016-07-25 10:16:34 -0700

    checking leetcode solutions till 237.

---
+++ leetcode/src/backtrack/Solution.java
@@ -0,0 +1,90 @@
+package backtrack;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+	
+	//helper class
+	public class ListNode {
+		  int val;
+		  ListNode next;
+		  ListNode(int x) { val = x; }
+		 }
+	
+	/**   22. Generate Parentheses **/
+	public List<String> generateParenthesis(int n) {
+        List<String> output =  new ArrayList<String>();
+        Set<String> in_processing = new HashSet<String>();
+        String cur_string = new String("()");
+        if(n> 0) 
+            call_func(n-1, cur_string, in_processing, output);
+        else output.add("");
+        
+        return output;
+    }
+    
+    public void call_func(int n, String s,  Set<String> in_processing, List<String> output) {
+        // stores as output if this is a new combination
+        if(n ==0) {
+            //if(!output.contains(s)) 
+            output.add(new String(s));
+            return;
+        }
+        //introduce one parenthesis pair create different combination parenthesis 
+        for(int i = 0; i <= s.length(); i++) {
+            //String s1 = s.substring(0, i) + "(" + s.substring(i);
+            s = s.substring(0, i) + "(" + s.substring(i);
+            for(int j = i+1; j <= s.length(); j++) {
+               // String s2 = s1.substring(0,j) + ")" + s1.substring(j);
+                 s = s.substring(0,j) + ")" + s.substring(j);
+                //recursively go down with s if this combination is not seen before
+                /**
+                if(!in_processing.contains(s2)) {
+                    in_processing.add(new String(s2));
+                    call_func(n-1, s2, in_processing, output);
+                    //call_func(n-1, s2, output);
+                }**/
+                if(!in_processing.contains(s)) {
+                    in_processing.add(new String(s));
+                    call_func(n-1, s, in_processing, output);
+                    //call_func(n-1, s2, output);
+                }
+                s = s.substring(0, j) + s.substring(j+1);
+            }
+            s = s.substring(0, i) + s.substring(i+1);    
+        }
+    }
+    
+    /** 20. Valid Parentheses **/
+    public boolean isValid(String s) {
+        List<String> stack = new ArrayList<String>();
+        for(int i = 0; i < s.length(); i++) {
+            String ch = new String(s.substring(i,i+1));
+            if(ch.equals("(") || ch.equals("{") || ch.equals("[")) {
+                stack.add(ch);
+            } else if(ch.equals(")")) {
+                if(stack.isEmpty()) return false;
+                if(!stack.remove(stack.size()-1).equals("(")) return false;
+            } else if(ch.equals("}")) {
+                if(stack.isEmpty()) return false;
+                if(!stack.remove(stack.size()-1).equals("{")) return false;
+            } else if(ch.equals("]")) {
+                if(stack.isEmpty()) return false;
+                if(!stack.remove(stack.size()-1).equals("[")) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    
+    /** 237. Delete Node in a Linked List **/
+    public void deleteNode(ListNode node) {
+        if(node.next != null) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+        }
+    }
+
+}
---
+++ leetcode/src/tree/Solution.java
@@ -0,0 +1,90 @@
+package tree;
+
+public class Solution {
+	
+	//Helper class
+	public class TreeNode {
+		     int val;
+		    TreeNode left;
+		    TreeNode right;
+		    TreeNode(int x) { val = x; }
+	}
+	
+	//100. Same Tree
+	public boolean isSameTree(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left == null && right != null || left != null && right == null) return false;
+        if(left.val != right.val) return false; 
+        //check symmetry of left of left and right of right 
+        if(!isSameTree(left.left , right.left)) return false; 
+        //check symmetry of right of left and left of right 
+        if(!isSameTree(left.right , right.right)) return false;
+        return true;
+    }
+	
+	//101. Symmetric Tree
+	public boolean is_symmetric(TreeNode left, TreeNode right) {
+        // check if left, right have identical values
+        if(left == null && right == null) return true;
+        if(left == null && right != null || left != null && right == null) return false;
+        if(left.val != right.val) return false; 
+        //check symmetry of left of left and right of right 
+        if(!is_symmetric(left.left , right.right)) return false; 
+        //check symmetry of right of left and left of right 
+        if(!is_symmetric(left.right , right.left)) return false;
+        return true;
+    }
+	
+	//104. Maximum Depth of Binary Tree
+	public int maxDepth(TreeNode root) {
+        if(root == null) return 0; // special first case 
+        int ldepth = 0, rdepth = 0;
+        //left subtree 
+        if(root.left != null) ldepth = maxDepth(root.left);
+        //right subtree
+        if(root.right != null) rdepth = maxDepth(root.right);
+        return Math.max(ldepth, rdepth) +1;
+    }
+	
+	
+	//110. Balanced Binary Tree
+	public boolean isBalanced(TreeNode root) {
+        return call_dfs(root) != -1;
+        //return (ht != -1);
+        
+    }
+    
+    public int call_dfs(TreeNode cur) {
+        if(cur == null) return 0; // special first case 
+        int ldepth = 0, rdepth = 0;
+        //left subtree 
+        if(cur.left != null) {
+            ldepth = call_dfs(cur.left);
+            if(ldepth == -1) return -1;
+        }
+        
+        //right subtree
+        if(cur.right != null) {
+            rdepth = call_dfs(cur.right);
+            if(rdepth == -1) return -1;
+        }
+        
+        if(Math.abs(ldepth - rdepth) > 1) return -1;
+        return Math.max(ldepth, rdepth) +1;
+        
+    }
+    
+    
+	
+	//111. Minimum Depth of Binary Tree   
+	public int minDepth(TreeNode root) {
+	        if(root == null) return 0; // special first case 
+	        if(root.left == null && root.right == null) return 1;
+	        if(root.right  == null) return minDepth(root.left) +1;
+	        if(root.left  == null) return minDepth(root.right) +1;
+	        int ldepth = minDepth(root.left), rdepth = minDepth(root.right);
+	        return ldepth <= rdepth ? ldepth +1 : rdepth + 1;
+	    }
+	
+
+}
